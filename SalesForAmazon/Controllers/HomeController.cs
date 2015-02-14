81dd746e44b244d210281241d99d9f356eec6c67
Ivan Tsurkan <tsurkan_i@mail.ru>
2015-02-14 23:38:20 +0200

    1.4 some changes

--- SalesForAmazon/Controllers/HomeController.cs
+++ SalesForAmazon/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using BookParser;
 using FluentSharp.CoreLib;
+using NLog.Fluent;
 using SalesForAmazon.Models;
 using Category = SalesForAmazon.Models.Category;
 using PagedList;
@@ -27,6 +28,7 @@ namespace SalesForAmazon.Controllers
         // GET: /Home/
         private List<Category> categoryList;
         readonly BookInfoEntities dbContext = new BookInfoEntities();
+        private readonly NLog.Logger Log = MvcApplication.logger;
 
         public void InitializeList()
         {
@@ -37,22 +39,47 @@ namespace SalesForAmazon.Controllers
             {
                 item.UrlName = Func.ParseURL(item.Name);
             }
+
+            ViewBag.dropdownCount = new SelectList(new Dictionary<string, int>
+            {
+                {"2", 2},
+                {"3", 3},
+                {"4", 4},
+                {"20", 20},
+                {"40", 40},
+                {"60", 60},
+                {"80", 80},
+                {"100", 100}
+            }, "Key", "Value");
+            ViewBag.counts = 4;
         }
 
         public ActionResult Index(string id, int? page, int? PageSize)
         {
+
+
+            if (ViewBag.count==null)
+            {
+                ViewBag.count = 4;
+            }
+            ViewBag.counts = (PageSize.isNotNull()) ? ViewBag.count : 4;
+            Log.Info("Creating new project from {0} at {1}", (int)ViewBag.counts, page);
+
             // todo установка категорій. треба потім занести все в базу
             //нафіга!?!?!? яякщо в базу то не треба парсити         \\ взагалі-то навіть не треба
             //або як зараз я зробив                                 \\ хотів зробити у базі таблицю categories
             //або перестати гамнокодити і зробити динамічно         \\ а потім занести у базу
             //хіба так хоче замовник                                \\ але категорії не змінюються, так що не треба
-            InitializeList();                                       
+            InitializeList();
             // просто для демонстрації
-            var books = from b in dbContext.Books
-                                      select b;
-            books = books.OrderBy(n => n.Name);
+            var books = from b in 
+                            dbContext.Books.
+                            OrderBy(n => n.Name)
+                            select b;
             var pageNumber = (page ?? 1);
-            var pageSize = (PageSize ?? 4);
+            Log.Info("Creating new project from {0} at {1}", (int)ViewBag.counts, page);
+          
+            var pageSize = (PageSize ?? (int)ViewBag.counts);
             var booksPaged = books.ToPagedList(pageNumber, pageSize);
 
             // треба передавати 2 моделі. 2-га для Partial
